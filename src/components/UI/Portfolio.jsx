a3dd6c050cd46ceda5ba20e763ef65cd23cf94aa
FROMERO63 <romerofrank63@gmail.com>
2023-11-22 01:48:20 -0800

    styled Projects

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -1,23 +1,124 @@
+import profilePhoto from './profilePic.jpg'
+
 export default function Portfolio() {
     return (
-      <section className="portfolioContainer">
-        <div>
-            Portfoliio Sections here
-        </div>
-        <div>
-            Portfoliio Sections here
-        </div>
-        <div>
-            Portfoliio Sections here
-        </div>
-        <div>
-            Portfoliio Sections here
-        </div>
-        <div>
-            Portfoliio Sections here
-        </div>
-        <div>
-            Portfoliio Sections here
+      <section id="portfolioContainer">
+        <div class="card" id="portfolioCard">
+            <header class="card-header" id="portfolioHeader">
+                <p class="card-header-title">Portfolio</p>
+            </header>
+          <div  id="portfolioContent">
+            <div class="columns is-multiline is-mobile">
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                        <div class="media-content">
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                        <div class="media-content">
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                        <div class="media-content">
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                        <div class="media-content" >
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-third">
+                <div class="card" id="projects">
+                    <div class="card-image">
+                        <figure class="image is-1by1">
+                            <img src={profilePhoto} alt="Placeholder image"></img>
+                        </figure>
+                    </div>
+                    <div class="card-content" id="cardBackground">
+                        <div class="media">
+                        <div class="media-content">
+                                <p class="projectTitle">Title Of Project</p>
+                                <p class="projectLink">@github.com</p>
+                                <p class="projectLink">@github.con/title-of-project</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
       </section>
     );
